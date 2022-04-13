563e54b1dd3be44a7f79e90c0265c7aa423e684b
aldoriandra31 <aldoriandra62@gmail.com>
2022-04-13 13:38:45 +0700

    perubahan 1

---
+++ resources/views/bilal_06/home.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Halaman Home</title>
+    <link rel="stylesheet" href="">
+</head>
+<body>
+    <h1>Data Mahasiswa</h1>
+
+    <a href="">Tambah Mahasiswa</a>
+    <br>
+    <br>
+    <table border="1">
+        <tr>
+            <td>No</td>
+            <td>Nama</td>
+            <td>Jurusan</td>
+            <td>Aksi</td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Aldoriandra Bilal Tarekh Mohammad</td>
+            <td>Teknik Informatika</td>
+            <td><a href="">Edit</a> | <a href="">Delete</a><td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Audya Ayuning Tyas</td>
+            <td>Manajemen Bisnis</td>
+            <td><a href="">Edit</a> | <a href="">Delete</a><td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Muhammad Zaki</td>
+            <td>Teknik Informatika</td>
+            <td><a href="">Edit</a> | <a href="">Delete</a><td>
+        </tr>
+    </table>
